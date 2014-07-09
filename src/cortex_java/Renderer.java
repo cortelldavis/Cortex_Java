b4c37d4dc4f989bc58f2fcf54d1443f605d2af6f
cortell davis <>
2014-07-09 11:07:18 -0500

    Refined Renderer class

--- src/cortex_java/Renderer.java
+++ src/cortex_java/Renderer.java
@@ -19,16 +19,31 @@ import java.awt.image.BufferedImage;
  */
 public class Renderer implements WorldListener {
 
-    BufferedImage activeImage, staticImage;
-    Graphics2D active_GO, static_GO;
+    BufferedImage activeImage, compositeLayer;
+    Graphics2D active_GO, composite_GO;
     World world;
 
-    BufferedImage worldLayer;
+    BufferedImage worldLayer[];
 
     public Renderer() {
 
-        
-        
+        worldLayer = new BufferedImage[10];
+
+        worldLayer[0] = new BufferedImage(96, 128, BufferedImage.TYPE_INT_ARGB);
+        //holds a single pritesheet
+        worldLayer[1] = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
+        //holds a single frame
+        worldLayer[2] = new BufferedImage(800, 600, BufferedImage.TYPE_INT_ARGB);
+        //holds a single local map
+
+        compositeLayer = new BufferedImage(800, 600, BufferedImage.TYPE_INT_ARGB);
+        //holds all layers into a composite
+
+        composite_GO = compositeLayer.createGraphics();
+        composite_GO.setColor(Color.black);
+        composite_GO.fillRect(0, 0, compositeLayer.getWidth(), compositeLayer.getHeight());
+        composite_GO.setColor(Color.white);
+        composite_GO.drawString("Composite Layer", 390, 290);
     }
 
     @Override
@@ -45,19 +60,20 @@ public class Renderer implements WorldListener {
     }
 
     void renderWorldObject(WorldObject worldObject) {
-
-        //worldObject.getPosition();
-        //worldObject.getSize();
-        //worldObject.isCollidable();
-        worldObject.getTexture();
-        active_GO.setColor(Color.black);
-        active_GO.fillRect(0, 0, activeImage.getWidth(), activeImage.getHeight());
-        active_GO.drawImage(ResourceLoader.getImage(worldObject.getTexture().getTextureAddress()), worldObject.getXPosition(), worldObject.getYPosition(), (int) (worldObject.getXPosition() + worldObject.getWidth()), (int) (worldObject.getYPosition() + worldObject.getHeight()), worldObject.getTexture().textureSource_x1, worldObject.getTexture().textureSource_y1, worldObject.getTexture().textureSource_x2, worldObject.getTexture().textureSource_y2, null);
-        static_GO.drawImage(activeImage, 0, 0, null);
+        /*
+         //worldObject.getPosition();
+         //worldObject.getSize();
+         //worldObject.isCollidable();
+         worldObject.getTexture();
+         active_GO.setColor(Color.black);
+         active_GO.fillRect(0, 0, activeImage.getWidth(), activeImage.getHeight());
+         active_GO.drawImage(ResourceLoader.getImage(worldObject.getTexture().getTextureAddress()), worldObject.getXPosition(), worldObject.getYPosition(), (int) (worldObject.getXPosition() + worldObject.getWidth()), (int) (worldObject.getYPosition() + worldObject.getHeight()), worldObject.getTexture().textureSource_x1, worldObject.getTexture().textureSource_y1, worldObject.getTexture().textureSource_x2, worldObject.getTexture().textureSource_y2, null);
+         static_GO.drawImage(activeImage, 0, 0, null);
+         */
     }
 
     public BufferedImage getCompositeRender() {
-        return new BufferedImage(800, 600, BufferedImage.TYPE_INT_ARGB);
+        return compositeLayer;
     }
 
     void setWorld(World target) {
