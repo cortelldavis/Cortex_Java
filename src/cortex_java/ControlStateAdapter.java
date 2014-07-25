cb5d4d55c3158fe6c777c59baadb8831475149c0
cortell <cortell@ubuntu-cortell>
2014-07-25 13:50:45 -0500

    Cortex_Client is being plugged into its controlstateadapter to allow seamless transitions between game states.

--- src/cortex_java/ControlStateAdapter.java
+++ src/cortex_java/ControlStateAdapter.java
@@ -6,34 +6,74 @@
  */
 package cortex_java;
 
+import cortex_java.Controller.Command;
+
 /**
  *
  * @author Cortell Davis <cortelldavis@gmail.com>
  */
 public class ControlStateAdapter {
     /*
-     * This class provides an interface betweenst the controller and state objects of the Control and GameState classes respectively. 
+     * This class provides an interface betweenst the controller and state objects of the Controller and GameState classes respectively. 
      */
 
-    Controller controller;
-    GameState state;
+    private Controller controller;
+    private GameState state;
 
     public ControlStateAdapter() {
         //contructor
-        
+
         /*
         
-        if controller is enabled
+         if controller is enabled
         
-        monitor inputs
+         monitor inputs
         
-        if inputs are active
+         if inputs are active
         
-        map them to the state, then respond wiht the approriate actions
-        
-        
+         map them to the state, then respond wiht the approriate actions
         
         
-        */
+        
+        
+         */
     }
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public GameState getState() {
+        return state;
+    }
+
+    public void setState(GameState state) {
+        this.state = state;
+    }
+
+    void loop() {
+        
+        
+        state.onStart();
+
+        if (controller.isKeyPressed()) {
+
+            if (controller.getCommand() == Command.UP) {
+            System.out.println("Up");
+            }
+            if (controller.getCommand() == Command.LEFT) {
+            System.out.println("Left");}
+            if (controller.getCommand() == Command.DOWN) {
+            System.out.println("Down");}
+            if (controller.getCommand() == Command.RIGHT) {
+            System.out.println("Right");}
+            controller.setKeyPressed(false);
+        }
+
+    }
+
 }
--- src/cortex_java/Cortex_Client.java
+++ src/cortex_java/Cortex_Client.java
@@ -16,101 +16,109 @@ import world.World;
  * @author cortell davis
  */
 public class Cortex_Client implements Runnable {
-    
-    private static boolean running = true;
-    
+
+    private Controller controller;
+    private static boolean running;
+    private World world;
+    private Renderer r;
+    private GameState clientState;
+    private ControlStateAdapter adapter;
+
     public static void main(String args[]) {
         new Thread(new Cortex_Client(), "Cortex_Client").start();
     }
-    
+
     @Override
-    
     public void run() {
-        World world = new World();
+        clientState = new GameState() {
+
+            @Override
+            public void onResume() {
+                super.onResume();
+            }
+
+            @Override
+            public void onExit() {
+                super.onExit();
+            }
+
+            @Override
+            public void onPause() {
+                super.onPause();
+            }
+
+            @Override
+            public void onStart() {
+                super.onStart();
+
+            }
+        };
+
+        world = new World();
         world.createWorld();
-        Renderer r = new Renderer();
-        
+        r = new Renderer();
         r.setWorld(world);
         View vw = new View();
         vw.setDisplayImage(r.getCompositeRender());
-        
-        Controller control = new Controller();
-        
+        controller = new Controller();
         vw.getDisplayPanel().setFocusable(true);
         vw.getDisplayPanel().requestFocusInWindow();
-        vw.getDisplayPanel().addKeyListener(control);
-        
+        vw.getDisplayPanel().addKeyListener(controller);
         vw.updateDisplay();
-        /*
-         for (int count = 1; count <= 3; count++) {
-         System.out.println("\nobject has been created in the world:\nID: " + world.getWorldObjectById(count).getId());
-         System.out.println("name: " + ((Actor) world.getWorldObjectById(count)).getActor_Name());
-         System.out.println("level: " + ((Actor) world.getWorldObjectById(count)).getLevel());
-         System.out.println("health: " + ((Actor) world.getWorldObjectById(count)).getHealth());
-         System.out.println("agility: " + ((Actor) world.getWorldObjectById(count)).getAgility());
-         System.out.println("strength: " + ((Actor) world.getWorldObjectById(count)).getStrength());
-         System.out.println("willpower: " + ((Actor) world.getWorldObjectById(count)).getWillpower());
-         System.out.println("World Object Properties:");
-         System.out.println("\n");
-         System.out.println("size x1: " + ((Actor) world.getWorldObjectById(count)).getSize().x);
-         System.out.println("size y1: " + ((Actor) world.getWorldObjectById(count)).getSize().y);
-         System.out.println("size x2: " + ((Actor) world.getWorldObjectById(count)).getSize().width);
-         System.out.println("size y2: " + ((Actor) world.getWorldObjectById(count)).getSize().height);
-         System.out.println("\n");
-         System.out.println("texture frame y1: " + ((Actor) world.getWorldObjectById(count)).getTexture().getTextureFrame_y1());
-         System.out.println("texture frame x1: " + ((Actor) world.getWorldObjectById(count)).getTexture().getTextureFrame_x1());
-         System.out.println("texture frame y2: " + ((Actor) world.getWorldObjectById(count)).getTexture().getTextureFrame_y2());
-         System.out.println("texture frame x2: " + ((Actor) world.getWorldObjectById(count)).getTexture().getTextureFrame_x2());
-         System.out.println("\n");
-         System.out.println("texture source y1: " + ((Actor) world.getWorldObjectById(count)).getTexture().getTextureSource_y1());
-         System.out.println("texture source x1: " + ((Actor) world.getWorldObjectById(count)).getTexture().getTextureSource_x1());
-         System.out.println("texture source y2: " + ((Actor) world.getWorldObjectById(count)).getTexture().getTextureSource_y2());
-         System.out.println("texture source x2: " + ((Actor) world.getWorldObjectById(count)).getTexture().getTextureSource_x2());
-         System.out.println("\n");
-         System.out.println("position x:" + ((Actor) world.getWorldObjectById(count)).getPosition().x);
-         System.out.println("position y:" + ((Actor) world.getWorldObjectById(count)).getPosition().y);
-         System.out.println("collidable: " + ((Actor) world.getWorldObjectById(count)).isCollidable());
-         System.out.println("\n");
-         }
-         */
-
-        //   world.getWorldMap().printMap();
-        GameState clientState = new GameState() {
-            
-            @Override
-            public void onResume() {
-                super.onResume(); //To change body of generated methods, choose Tools | Templates.
-            }
-            
-            @Override
-            public void onExit() {
-                super.onExit(); //To change body of generated methods, choose Tools | Templates.
-            }
-            
-            @Override
-            public void onPause() {
-                super.onPause(); //To change body of generated methods, choose Tools | Templates.
-            }
-            
-            @Override
-            public void onStart() {
-                super.onStart(); //To change body of generated methods, choose Tools | Templates.
-            }
-        };
-        
         clientState.setState(clientState.START);
-        
+        adapter = new ControlStateAdapter();
+        adapter.setState(clientState);
+        adapter.setController(controller);
+        running = true;
+
         while (running) {
-            
-            //controller
-            //clientState
-            //CommandStateAdapter
-            
+
+            adapter.loop();
+
             try {
                 Thread.sleep(30);
             } catch (InterruptedException ex) {
                 Logger.getLogger(Cortex_Client.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+
+        debug_code:
+        {
+            /*
+             for (int count = 1; count <= 3; count++) {
+             System.out.println("\nobject has been created in the world:\nID: " + world.getWorldObjectById(count).getId());
+             System.out.println("name: " + ((Actor) world.getWorldObjectById(count)).getActor_Name());
+             System.out.println("level: " + ((Actor) world.getWorldObjectById(count)).getLevel());
+             System.out.println("health: " + ((Actor) world.getWorldObjectById(count)).getHealth());
+             System.out.println("agility: " + ((Actor) world.getWorldObjectById(count)).getAgility());
+             System.out.println("strength: " + ((Actor) world.getWorldObjectById(count)).getStrength());
+             System.out.println("willpower: " + ((Actor) world.getWorldObjectById(count)).getWillpower());
+             System.out.println("World Object Properties:");
+             System.out.println("\n");
+             System.out.println("size x1: " + ((Actor) world.getWorldObjectById(count)).getSize().x);
+             System.out.println("size y1: " + ((Actor) world.getWorldObjectById(count)).getSize().y);
+             System.out.println("size x2: " + ((Actor) world.getWorldObjectById(count)).getSize().width);
+             System.out.println("size y2: " + ((Actor) world.getWorldObjectById(count)).getSize().height);
+             System.out.println("\n");
+             System.out.println("texture frame y1: " + ((Actor) world.getWorldObjectById(count)).getTexture().getTextureFrame_y1());
+             System.out.println("texture frame x1: " + ((Actor) world.getWorldObjectById(count)).getTexture().getTextureFrame_x1());
+             System.out.println("texture frame y2: " + ((Actor) world.getWorldObjectById(count)).getTexture().getTextureFrame_y2());
+             System.out.println("texture frame x2: " + ((Actor) world.getWorldObjectById(count)).getTexture().getTextureFrame_x2());
+             System.out.println("\n");
+             System.out.println("texture source y1: " + ((Actor) world.getWorldObjectById(count)).getTexture().getTextureSource_y1());
+             System.out.println("texture source x1: " + ((Actor) world.getWorldObjectById(count)).getTexture().getTextureSource_x1());
+             System.out.println("texture source y2: " + ((Actor) world.getWorldObjectById(count)).getTexture().getTextureSource_y2());
+             System.out.println("texture source x2: " + ((Actor) world.getWorldObjectById(count)).getTexture().getTextureSource_x2());
+             System.out.println("\n");
+             System.out.println("position x:" + ((Actor) world.getWorldObjectById(count)).getPosition().x);
+             System.out.println("position y:" + ((Actor) world.getWorldObjectById(count)).getPosition().y);
+             System.out.println("collidable: " + ((Actor) world.getWorldObjectById(count)).isCollidable());
+             System.out.println("\n");
+             }
+             */
+
+            //   world.getWorldMap().printMap();
+        }
+
     }
 }
