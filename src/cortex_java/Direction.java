f322499fda74ae5fa0236c477da7c6574ca8d5d3
cortell <cortell@ubuntu-cortell>
2014-07-26 03:47:48 -0500

    Revert "Map class has been created"
    This reverts commit faa092830da0561fb5289dbd90cbe5c2123df882.

--- src/cortex_java/Direction.java
+++ src/cortex_java/Direction.java
@@ -1,8 +1,7 @@
-/* 
- *  You must contact the author of this material, and then ask for permission, before accepting the terms of this license.
- *  You are not free to copy and redistribute the material in any medium or format.
- *  You may view the material only to contribute to the material. 
- *  You may  not use the material in any commercial way.
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package cortex_java;
 
--- src/world/WorldObject.java
+++ src/world/WorldObject.java
@@ -1,9 +1,4 @@
-/* 
- *  You must contact the author of this material, and then ask for permission, before accepting the terms of this license.
- *  You are not free to copy and redistribute the material in any medium or format.
- *  You may view the material only to contribute to the material. 
- *  You may  not use the material in any commercial way.
- */
+
 package world;
 
 import cortex_java.Direction;
